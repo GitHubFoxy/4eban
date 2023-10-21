4a301202bd3c3fbb11e65363eea07260cd862140
windie <ambrosayos@mail.ru>
2023-10-21 13:11:48 +0700

    Laba 10-11

--- www/mvc/subscribe.php
+++ www/mvc/subscribe.php
@@ -1,3 +1,21 @@
 <?php
-
+require ("model.php");
+class ContactPage extends Model {
+//Перекрытие значений атрибутоа родительского класса
+Public $title = "Алтай-Тур - Подписка";
+Public $description = "Алтай-Тур - Подписка";
+function Display() {
+$this -> DisplayTitle();
+$this -> DisplayDescriptions();
+$this -> DisplayStyles();
+$this -> DisplayHeader();
+$this -> DisplayMenu($this->buttons);
+$this->DisplayContent($this->filename);
+$this -> DisplayFooter();
+}
+}
+$contact = new ContactPage();
+$contact -> filename = "view/subscribe.html";
+$contact -> Display();
+require './subscribe/subscribe_add.inc';
 ?>
